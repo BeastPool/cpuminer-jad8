895e3d4a06629eac69b062c4eee06a1d7047a8de
xcouiz <xcouiz@gmail.com>
2018-08-01 12:53:51 +0200

    jad8

--- winbuild-cross.sh
+++ winbuild-cross.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-LOCAL_LIB="$HOME/usr/lib"
+LOCAL_LIB="$HOME/win64"
 
-export LDFLAGS="-L$LOCAL_LIB/curl/lib/.libs -L$LOCAL_LIB/gmp/.libs -L$LOCAL_LIB/openssl"
-
+export LDFLAGS="-L$LOCAL_LIB/curl/lib -L$LOCAL_LIB/gmp/lib -L$LOCAL_LIB/openssl/lib"
+export CPPFLAGS="-I$LOCAL_LIB/gmp/include"
 F="--with-curl=$LOCAL_LIB/curl --with-crypto=$LOCAL_LIB/openssl --host=x86_64-w64-mingw32"
 
 sed -i 's/"-lpthread"/"-lpthreadGC2"/g' configure.ac
 
 mkdir release
 cp README.txt release/
-cp /usr/x86_64-w64-mingw32/lib/zlib1.dll release/
+cp $LOCAL_LIB/zlib/bin/zlib1.dll release/
 cp /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll release/
 cp /usr/lib/gcc/x86_64-w64-mingw32/5.3-win32/libstdc++-6.dll release/
 cp /usr/lib/gcc/x86_64-w64-mingw32/5.3-win32/libgcc_s_seh-1.dll release/
-cp $LOCAL_LIB/openssl/libcrypto-1_1-x64.dll release/
-cp $LOCAL_LIB/curl/lib/.libs/libcurl-4.dll release/
+cp $LOCAL_LIB/openssl/bin/libcrypto-1_1-x64.dll release/
+cp $LOCAL_LIB/curl/bin/libcurl-4.dll release/
 
 make distclean || echo clean
 rm -f config.status
 ./autogen.sh || echo done
 CFLAGS="-O3 -march=core-avx2 -msha -Wall" ./configure $F
-make 
+make -j4
 strip -s cpuminer.exe
 mv cpuminer.exe release/cpuminer-avx2-sha.exe
 
@@ -34,7 +34,7 @@ mv cpuminer.exe release/cpuminer-avx2-sha.exe
 make clean || echo clean
 rm -f config.status
 CFLAGS="-O3 -march=core-avx2 -Wall" ./configure $F 
-make 
+make -j4
 strip -s cpuminer.exe
 mv cpuminer.exe release/cpuminer-avx2.exe
 
@@ -49,7 +49,7 @@ mv cpuminer.exe release/cpuminer-avx2.exe
 make clean || echo clean
 rm -f config.status
 CFLAGS="-O3 -march=corei7-avx -Wall" ./configure $F 
-make 
+make -j4
 strip -s cpuminer.exe
 mv cpuminer.exe release/cpuminer-avx.exe
 
@@ -58,7 +58,7 @@ make clean || echo clean
 rm -f config.status
 CFLAGS="-O3 -march=westmere -Wall" ./configure $F
 #CFLAGS="-O3 -maes -msse4.2 -Wall" ./configure $F
-make 
+make -j4
 strip -s cpuminer.exe
 mv cpuminer.exe release/cpuminer-aes-sse42.exe
 
@@ -80,7 +80,7 @@ mv cpuminer.exe release/cpuminer-aes-sse42.exe
 make clean || echo clean
 rm -f config.status
 CFLAGS="-O3 -msse2 -Wall" ./configure $F
-make
+make -j4
 strip -s cpuminer.exe
 mv cpuminer.exe release/cpuminer-sse2.exe
 make clean || echo clean
